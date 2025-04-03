5f897550463486791b2f7d37727cd2228808a0cc
Jatin Sharma <jatinsharma1364@gmail.com>
2025-04-03 11:32:15 +0530

    updates

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
 
         <div>
-            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <img src={assets.logo} className='mb-5 w-40' alt="" />
             <p className='w-full md:w-2/3 text-gray-600'>
             Forever is a fashion destination that brings timeless style to life. Our brand celebrates individuality, offering high-quality, 
             trend-forward clothing designed for every moment, making you look and feel your best—today, tomorrow, and forever.
@@ -28,7 +28,7 @@ const Footer = () => {
             <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
             <ul className='flex flex-col gap-1 text-gray-600'>
                 <li>+1-212-456-7890</li>
-                <li>admin@jatinsharma.in</li>
+                <li>admin@smartkart.in</li>
             </ul>
         </div>
 
@@ -36,7 +36,7 @@ const Footer = () => {
 
         <div>
             <hr />
-            <p className='py-5 text-sm text-center'>Copyright 2024@ jatinsharma.in - All Right Reserved.</p>
+            <p className='py-5 text-sm text-center'>Copyright 2025@ smartkart.in - All Right Reserved.</p>
         </div>
 
     </div>
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -15,9 +15,9 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className=' text-gray-500'>B-45/47 Block-B <br /> Cannaught Place, Delhi, India</p>
-          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: admin@jatinsharma.in</p>
-          <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
+          <p className=' text-gray-500'>B-45/47 Block-B <br /> Mohali, Punjab, India</p>
+          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: admin@smartkart.in</p>
+          <p className='font-semibold text-xl text-gray-600'>Careers at SmartKart</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
